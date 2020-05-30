effb84d900591ec3ddc009d2f293522046c1a5d9
lnpbqc <60287061+lnpbqc@users.noreply.github.com>
2020-05-30 17:45:50 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ theRight();
 
 
 // 时间
+
 function time() {
     var date = new Date();
     var hour = date.getHours();
@@ -105,63 +106,6 @@ setInterval(countDown,1000);
 
 
 
-// 歌曲控制
-// 借鉴：https://www.open-open.com/lib/view/open1407401112973.html
-var songs = document.querySelector('audio');
-var volume_plus = document.querySelector('#songs_control').querySelector('.plus');
-var volume_less = document.querySelector('#songs_control').querySelector('.less');
-var songs_next = document.querySelector('#songs_control').querySelector('.next');
-var songs_stop = document.querySelector('#songs_control').querySelector('.stop');
-var songs_number = 0;
-songs.volume = 0.25;
-songs_next.onclick = function(){
-    if(songs_number == 0){
-        // 青春大概
-        songs_number += 1;
-        songs.src = 'http://music.163.com/song/media/outer/url?id=1387568493.mp3';
-    }else if(songs_number == 1){
-        // Illusionary Daytime
-        songs_number += 1;
-        songs.src = 'http://music.163.com/song/media/outer/url?id=28907016.mp3';
-    }else if(songs_number == 2){
-        // 倒数
-        songs_number += 1;
-        songs.src = 'http://music.163.com/song/media/outer/url?id=1299550532.mp3';
-    }else{
-        // 句号
-        songs_number = 0;
-        songs.src = 'http://music.163.com/song/media/outer/url?id=1405283464.mp3';
-    }
-}
-var songs_stopnum = true;
-songs_stop.onclick = function(){
-    if(songs_stopnum){
-        songs.play();
-        songs_stopnum = false;
-    }else{
-        songs.pause();
-        songs_stopnum = true;
-    }
-    if(songs_stopnum == true){
-        songs_stop.innerHTML = 'Play';
-    }else{
-        songs_stop.innerHTML = 'Stop';
-    }
-}
-volume_plus.onclick = function(){
-    if(songs.volume == 1){
-        console.log('WARNING!');
-    }
-    songs.volume = songs.volume + 0.25;
-}
-volume_less.onclick = function(){
-    if(songs.volume == 0){
-        console.log('WARNING!');
-    }
-    songs.volume = songs.volume - 0.25;
-}
-
-
 // 搜索
 var searchbtn = document.getElementById('tosearch');
 var search = document.getElementById('search');
@@ -201,7 +145,26 @@ document.addEventListener('keyup',function(e){
     }
 })
 
-// 阻止复制
+// blog部分
+var blog = document.querySelector('#blog').querySelector('#blogMain');
+var blog_dairy = blog.children[0];
+    blog_dairy.onclick = function(){
+        location.replace('/links/dairy/');
+    }
+    blog_dairy.querySelector('h4').innerHTML = '日记';
+    blog_dairy.querySelector('h4').style.fontFamily = '楷体';
+    blog_dairy.querySelector('h4').style.color = 'white';
+    blog_dairy.querySelector('h4').style.textShadow = '0 0 20px gray';
+    blog_dairy.querySelector('#blogIntroduction').innerHTML = '似乎一般人都不会写日记的,doge';
+var blog_index;
+
+// 阻止复制、防止窥探
+document.addEventListener('keydown',function(e){
+    // console.log(e.keyCode);
+    if(e.keyCode == 123){
+        location.replace('https://www.baidu.com/');
+    }
+})
 document.addEventListener('contextmenu',function(e){
     e.preventDefault();
 })
