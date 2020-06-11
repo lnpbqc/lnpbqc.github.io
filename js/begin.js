9a7bbe8b47f7e746a8d3ce206c332b5a2cf9e973
lnpbqc <60287061+lnpbqc@users.noreply.github.com>
2020-06-11 23:06:27 +0800

    Update begin.js

--- js/begin.js
+++ js/begin.js
@@ -1,18 +1,19 @@
 // confirm('本页面有音乐，请调低音量！！');
-// function IsPC() {
-//     var userAgentInfo = navigator.userAgent;
-//     var Agents = ["Android", "iPhone","SymbianOS", "Windows Phone","iPad", "iPod"];
-//     var flag = true;
-//         for (var v = 0; v < Agents.length; v++) {
-//             if (userAgentInfo.indexOf(Agents[v]) >= 0) {
-//                 flag = false;
-//                 break;
-//             }
-//         }
-//     return flag;
-// }
-// var flag = IsPC(); //true为PC端，false为手机端
-// // console.log(flag);
-// if(flag != true){
-//     location.replace("404");
-// }
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone","SymbianOS", "Windows Phone","iPad", "iPod"];
+    var flag = true;
+        for (var v = 0; v < Agents.length; v++) {
+            if (userAgentInfo.indexOf(Agents[v]) >= 0) {
+                flag = false;
+                break;
+            }
+        }
+    return flag;
+}
+var flag = IsPC(); //true为PC端，false为手机端
+// console.log(flag);
+if(flag != true){
+    alert("WELCOME EVERYONE!");
+    location.replace("404");
+}
