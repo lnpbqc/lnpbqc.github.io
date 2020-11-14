aad78cd58f51df7b14fffc3015c1dff05de2a10b
lnpbqc <60287061+lnpbqc@users.noreply.github.com>
2020-11-14 15:30:28 +0800

    Update begin.js

--- js/begin.js
+++ js/begin.js
@@ -17,3 +17,17 @@ if(flag != true){
     // location.replace("404");
     alert('暂时没能力弄移动端，推荐PC端');
 }
+var time = new Date();
+var hour = time.getHours();
+console.log(hour);
+if(23< hour && hour <4){
+    alert("怎么还没睡啊，少年(女)？");
+}else if(6<= hour && hour <11){
+    alert("上午好!");
+}else if(11<= hour && hour <= 13){
+    alert("午好啊，吃了午饭没？");
+}else if(13 < hour && hour <= 18){
+    alert("下午好！");
+}else {
+    alert('晚上好，没事的话，早点睡吧。')
+}
