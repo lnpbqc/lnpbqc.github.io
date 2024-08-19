9e2ac0ada7d821635ae5383d043e27417c39724d
lnpbqc <1264776510@qq.com>
2024-08-19 10:39:49 +0800

    Site updated: 2024-08-19 10:39:46

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/c2835fa6.html","posts/2f28f7d1.html","posts/16f2d8ad.html","posts/755f3e3d.html","posts/efd5f3e1.html"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["posts/4a17b156.html","posts/c2835fa6.html","posts/2f28f7d1.html","posts/755f3e3d.html","posts/16f2d8ad.html","posts/efd5f3e1.html"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
